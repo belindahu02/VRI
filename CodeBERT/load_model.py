651802abe520f3bb3b8e793e786dd499989faa1a
Belinda Hu <behu3179@uni.sydney.edu.au>
2024-01-18 11:11:16 +1300

    change to unixcoder

--- CodeBERT/load_model.py
+++ CodeBERT/load_model.py
@@ -3,8 +3,15 @@ import torch
 import h5py
 from transformers import AutoModel, AutoTokenizer
 
-model_name = "microsoft/codebert-base"
+model_name = "microsoft/unixcoder-base"
+# model_name = "microsoft/codebert-base"
+
 output_folder = "CodeBERT/models"
+output_name_pth = "unixcoder_model.pth"
+output_name_h5 = "unixcoder_model.h5"
+# output_name_pth = "codebert_model.pth"
+# output_name_h5 = "codebert_model.h5"
+
 os.makedirs(output_folder, exist_ok=True)
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -12,7 +19,7 @@ model = AutoModel.from_pretrained(model_name)
 
 model.save_pretrained(output_folder)
 
-torch.save(model.state_dict(), os.path.join(output_folder, 'codebert_model.pth'))
-with h5py.File(os.path.join(output_folder, 'codebert_model.h5'), 'w') as hf:
+torch.save(model.state_dict(), os.path.join(output_folder, output_name_pth))
+with h5py.File(os.path.join(output_folder, output_name_h5), 'w') as hf:
     for key, value in model.state_dict().items():
         hf.create_dataset(key, data=value.cpu().numpy())
